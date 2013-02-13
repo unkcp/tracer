da73987444fab76baa2c65d87c48fcc38b9e358e
UnkcpZ <unkcp.yu@me.com>
2013-02-14 00:09:32 +0800

    Generated the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracer::Application.config.secret_token = 'e3c84165a5ce941e9c373b87c55f99da78a899eb3f79bfa3ed14514611f155673fc7e41f2b39f55b19df9697d9629abbe803c66fa3df9ecfdc83e81a03713620'
